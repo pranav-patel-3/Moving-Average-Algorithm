249da66c4463ebfa45498db77550743d4194fdef
Pranav Patel <Pranav.Patel@trigyn.com>
2023-04-12 14:37:09 +0530

    Add project files.

---
+++ Moving-Average-Algorithm/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Moving_Average_Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, let's begin, shree ganeshay namah :)..");
+
+            double[] sharePrices = { 1,2,3,4,5 };
+            double[] movingAverages = CalculateMovingAverage(sharePrices, 5);
+
+        }
+        public static double[] CalculateMovingAverage(double[] prices, int period)
+        {
+            double[] movingAverages = new double[prices.Length];
+
+            for (int i = period - 1; i < prices.Length; i++)
+            {
+                double sum = 0;
+
+                for (int j = 0; j < period; j++)
+                {
+                    sum += prices[i - j];
+                }
+
+                movingAverages[i] = sum / period;
+            }
+
+            return movingAverages;
+        }
+    }
+
+
+
+
+}
